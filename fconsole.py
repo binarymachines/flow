7dff088ec7507cf71f6bd09b18d61cb184ee846b
Dexter Taylor <binarymachineshop@gmail.com>
2021-03-04 00:04:59 -0500

    refining grammar spec

--- fconsole.py
+++ fconsole.py
@@ -3,7 +3,11 @@
 '''
 Usage:
     fconsole -g <grammar_file>
+    fconsole -g <grammar_file> -f <code_file>
 
+Options:
+    -g --grammar  Construct a parse tree from the specified grammar file
+    -f --flowcode  Parse the code in the specified file
 '''
 
 
@@ -26,7 +30,15 @@ def main(args):
         flow_grammar = f.read()
 
     parse_tree = Lark(flow_grammar, parser='lalr', debug=True)
+    parse_data = None
 
+    if args['--flowcode']:
+        codefile = args['<code_file>']
+        with open(codefile, 'r') as f:
+            code = f.read()
+            parse_data = parse_tree.parse(code)
+
+    print(parse_data)
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
---
+++ sample.f
@@ -0,0 +1,5 @@
+
+
+myform: { 
+    foo: "bar" 
+}
